9e1e3321ebb082b609ea4d978e2d930af7f520b2
Jailson <jailsonrodriguestech@gmail.com>
2024-05-06 17:35:33 -0300

    Calculadora do imc funcionando

--- src/main/resources/static/script/imc.js
+++ src/main/resources/static/script/imc.js
@@ -1,45 +1,55 @@
+
 function calcularIMC(peso, altura) {
 // Recebendo entrada do usuário no html
 peso = parseFloat(document.getElementById('weight').value);
 altura = parseFloat(document.getElementById('height').value);
 
-// Verificando se as entradas são válidas
-if (peso =="" || altura=="" || peso<0 || altura<0) {
-    alert("Por favor, insira valores numéricos válidos.");
+// Verificar se os valores inseridos são válidos
+    if (isNaN(peso) || isNaN(altura) || peso <= 0 || altura <= 0) {
+        alert('Por favor, insira valores válidos para peso e altura.');
+        return;
+    }
+
+// Calculando o IMC
+imc = peso / Math.pow(altura, 2);
+
+//saída do resultado no <h3>
+ document.getElementById('saida1').textContent = 'Seu IMC é: ' + imc.toFixed(2);
+
+var resultado;
+var cor;
+var fundo;
+
+if (imc < 18.5) {
+    fundo = "#f3ff85";
+    cor = "#000000";
+    resultado = 'Abaixo do peso';
+} else if (imc < 25) {  
+    fundo = "#5ee651";
+    cor = "#fff";  
+    resultado = 'Peso normal';
+} else if (imc < 30) {
+    fundo ="#952eda";;
+    cor = "#dfff29"; 
+    resultado = 'Sobrepeso';
+} else {
+    fundo = "#da0c0c";
+    cor = "#ffffff";
+    resultado = 'Obesidade';
 }
 
-    // Calculando o IMC
-    resultado = peso / Math.pow(altura, 2);
-
-   // saída no html
-    document.getElementById('saida1').innerHTML = "Seu Imc é: " + resultado.toFixed(2);
-
-if (resultado < 18.5) {
-        background.style.backgroundColor = "#f3ff85";
-        color.style.color = "#000000";
-        return situacao.innerHTML = "Situação: MAGREZA";  
-
-    } else if (resultado >= 18.5 && resultado < 25) {
-        background.style.backgroundColor= "#5ee651";
-        color.style.color= "#fff";
-        return situacao.innerHTML = "Situação: NORMAL";  
-
-    } else if (resultado >= 25 && resultado < 30) {
-        background.style.backgroundColor= "#952eda";
-        color.style.color= "#dfff29";
-        return situacao.innerHTML= "Situação: SOBREPESO"; 
-
-    } else{
-        background.style.backgroundColor = "#da0c0c";
-        color.style.color= "#ffffff";
-        return situacao.innerHTML = "Situação: OBESIDADE";  
-    }  
-
+// Atualizar estilos CSS do elemento
+var saida2 = document.getElementById('saida2');
+saida2.textContent = 'Classificação: ' + resultado;
+saida2.style.backgroundColor = fundo;
+saida2.style.color = cor;
 }
 
 function limparCampos() {
     document.getElementById('weight').value = '';
     document.getElementById('height').value = '';
-    document.getElementById('saida1').innerHTML = '';
-    document.getElementById('saida2').innerHTML = '';
-    }
+    document.getElementById('saida1').textContent = '';
+    document.getElementById('saida2').textContent = '';
+    saida2.style.backgroundColor="";
+
+}
